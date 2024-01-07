9a38339db83ead4cce53dc74bfd97fdbc7771627
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2024-01-07 17:09:15 +0200

    update

--- routes/courses.route.js
+++ routes/courses.route.js
@@ -3,25 +3,25 @@ const router = express.Router();
 const coursesController = require("../controllers/corses.controllers");
 const { body } = require("express-validator");
 
-router.get("/", coursesController.getAllCourses);
+router
+  .route("/")
+  .get(coursesController.getAllCourses)
+  .post(
+    [
+      body("title")
+        .notEmpty()
+        .withMessage("title is required")
+        .isLength({ min: 3 })
+        .withMessage("title at least 3 digits"),
+      body("price").notEmpty().withMessage("price is required"),
+    ],
+    coursesController.addCourse
+  );
 
-router.get("/:courseId", coursesController.getSingleCourse);
-
-router.post(
-  "/",
-  [
-    body("title")
-      .notEmpty()
-      .withMessage("title is required")
-      .isLength({ min: 3 })
-      .withMessage("title at least 3 digits"),
-    body("price").notEmpty().withMessage("price is required"),
-  ],
-  coursesController.addCourse
-);
-
-router.patch("/:courseId", coursesController.updateCourse);
-
-router.delete("/:courseId", coursesController.deleteCourse);
+router
+  .route("/:courseId")
+  .get(coursesController.getSingleCourse)
+  .patch(coursesController.updateCourse)
+  .delete(coursesController.deleteCourse);
 
 module.exports = router;
