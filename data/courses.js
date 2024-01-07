41d34e461eb043b439ee324614ef2c3c09c37c48
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2024-01-07 16:49:59 +0200

    create project

---
+++ data/courses.js
@@ -0,0 +1,20 @@
+let courses = [
+  {
+    id: 1,
+    title: "JavaScript Course",
+    price: 1000,
+  },
+  {
+    id: 2,
+    title: "React Course",
+    price: 500,
+  },
+  {
+    id: 3,
+    title: "Vue Course",
+    price: 300,
+  },
+];
+module.exports = {
+  courses,
+};
