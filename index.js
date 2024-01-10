f1d380ec54f4cbb70aaa52bd0341de5f19306cd8
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2024-01-10 19:35:16 +0200

    add cors file

--- index.js
+++ index.js
@@ -4,23 +4,21 @@ const app = express();
 app.use(express.json());
 const url = process.env.MONGO_URL;
 const httpStatusText = require("./utils/httpStatus");
+const cors = require("cors");
 const mongoose = require("mongoose");
 mongoose.connect(url).then(() => {
   console.log("mongodb connected successfully");
 });
 const port = process.env.PORT;
-
 const coursesRouter = require("./routes/courses.route");
-
+app.use(cors());
 app.use("/api/courses", coursesRouter);
-
 app.all("*", (req, res) => {
   return res.status(404).json({
     status: httpStatusText.ERROR,
     message: "This Resource Is Not Available",
   });
 });
-
 app.listen(port, () => {
   console.log(`Listining On Port: ${port}`);
 });
