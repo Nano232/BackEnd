046810b3b7ab46ca16455249a4c51c46d5805abb
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2024-01-10 19:13:48 +0200

    add .env file

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
-const url =
-  "mongodb+srv://elzoranymohamed232:23201823@learn-mongodb.imnrfk8.mongodb.net/codeZone?retryWrites=true&w=majority";
+const url = process.env.MONGO_URL;
+const httpStatusText = require("./utils/httpStatus");
 const mongoose = require("mongoose");
 mongoose.connect(url).then(() => {
   console.log("mongodb connected successfully");
 });
-const port = 5050;
+const port = process.env.PORT;
 
 const coursesRouter = require("./routes/courses.route");
 
 app.use("/api/courses", coursesRouter);
+
+app.all("*", (req, res) => {
+  return res.status(404).json({
+    status: httpStatusText.ERROR,
+    message: "This Resource Is Not Available",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Listining On Port: ${port}`);
