014dd0299140cf56a57da1d08dc647b27e0bd714
‪Mohamed Elzorkany <86691917+Nano232@users.noreply.github.com>
2024-01-22 19:17:19 +0200

    token

--- middlewares/verfiyToken.js
+++ middlewares/verfiyToken.js
@@ -3,9 +3,15 @@ const jwt = require("jsonwebtoken");
 const verfiyToken = (req, res, next) => {
   const authHeader =
     req.headers["Authorization"] || req.headers["authorization"];
+  if (!authHeader) {
+    return res.status(401).json("token is required");
+  }
   const token = authHeader.split(" ")[1];
-  const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-  console.log(decodedToken);
-  next();
+  try {
+    jwt.verify(token, process.env.JWT_SECRET_KEY);
+    next();
+  } catch (err) {
+    return res.status(401).json("invalid token");
+  }
 };
 module.exports = verfiyToken;
